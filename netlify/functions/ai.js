9c697a9f494dff5e894488c311016736b3f89187
Obanex <69535465+obanex@users.noreply.github.com>
2026-05-27 03:42:11 -0400

    Fix export syntax for handler function

--- netlify/functions/ai.js
+++ netlify/functions/ai.js
@@ -1,4 +1,4 @@
-export const handler = async (event) => {
+exports.handler = async (event) => {
   const headers = {
     "Access-Control-Allow-Origin": "*",
     "Content-Type": "application/json",
